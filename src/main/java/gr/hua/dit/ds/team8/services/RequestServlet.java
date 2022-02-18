952ecaa0a96a44464a2487f10943bfc6173eca4d
gakias <stavr_geo@hotmail.com>
2022-02-18 19:43:59 +0200

    -

--- src/main/java/gr/hua/dit/ds/team8/services/RequestServlet.java
+++ src/main/java/gr/hua/dit/ds/team8/services/RequestServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet("/letter_check")
+//@WebServlet("/Request")
 public class RequestServlet extends HttpServlet {
 
     @Autowired
@@ -27,27 +27,28 @@ public class RequestServlet extends HttpServlet {
         String st_ln = request.getParameter("student_ln");
         String t_fn = request.getParameter("teacher_fn");
         String t_ln = request.getParameter("teacher_ln");
-        // String purp = request.getParameter("purpose");
-        //String marks = request.getParameter("marks");
+        String purp = request.getParameter("purpose");
+        String marks = request.getParameter("marks");
+
         String email = request.getParameter("email");
 
-
         Letter letter = new Letter();
-
         letter.setStudent_fn(st_fn);
         letter.setStudent_ln(st_ln);
         letter.setTeacher_fn(t_fn);
         letter.setTeacher_ln(t_ln);
+       // letter.setMarks();
+       // letter.setEmail(email);
         letter.setEmail(email);
         letter.setApproved(false);
         letterRepository.save(letter);
-/*
+
         System.out.println(letter.getStudent_ln());
         System.out.println(letter.getStudent_fn());
         System.out.println(letter.getTeacher_fn());
         System.out.println(letter.getTeacher_ln());
         System.out.println(letter.getEmail());
-*/
+
     }
 
 }
