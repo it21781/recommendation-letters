83b6271a9549d6116a6ff6aa3537d47628b01d64
it21781 <it21781@hua.gr>
2022-02-21 14:50:37 +0200

    -

--- src/main/java/gr/hua/dit/ds/team8/repository/LetterRepository.java
+++ src/main/java/gr/hua/dit/ds/team8/repository/LetterRepository.java
@@ -2,18 +2,9 @@ package gr.hua.dit.ds.team8.repository;
 
 import gr.hua.dit.ds.team8.entity.Letter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "letters")
 public interface LetterRepository extends JpaRepository<Letter, Integer> {
-
-    @Modifying
-    @Query("update letters set approved = 1 where id = :id")
-    static void updateApproved(@Param(value = "id") String id) {
-        System.out.println("Niiiiiiiiiiiice");
-    }
 
 }
